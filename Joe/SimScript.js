b88d4db4301b4957919de978ec8f393c2b00695d
JoeTobon <jet33@njit.edu>
2018-03-19 21:12:04 -0400

    Update SimScript.js

--- Joe/SimScript.js
+++ Joe/SimScript.js
@@ -3,7 +3,6 @@ function f()
     //pointers to textboxes
     var ptrUser = document.getElementById("user")
     var ptrPass = document.getElementById("pass")
-    var ptrAmo  = document.getElementById("amount") 
   	
     //checks to see if user or pass is empty
     if(ptrUser.value.trim() == "" || ptrPass.value.trim() == "")
