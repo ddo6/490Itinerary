62e8661dcf80fc0e679c1aeb5e8acdd1759fc95d
ddo6 <ddo6@njit.edu>
2018-03-20 18:07:37 -0400

    Add files via upload

--- Midterm/register/register.php
+++ Midterm/register/register.php
@@ -1,1 +1,25 @@
+<?php
 
+  include ("Register-functions.php");
+  include ("account.php");
+  
+  ($dbh = mysql_connect ($hostname, $username, $password)) or die ("Unable to connect to MySQL database");
+  		
+  mysql_selectdb($project);
+
+  print "Successfully connected to MySQL. <br><br>";
+  
+  //GETS INPUT
+  $name = $_GET["name"];
+  $user = $_GET["user"];
+  $pass = $_GET["pass"];
+  $con  = $_GET["conPass"];
+  
+  print "name '$name' user '$user' pass '$pass' <br><br>"; 
+
+  user($user);  
+  
+  update($name, $user, $pass);
+  
+  print "<br><br>This interaction is completed";
+?>
