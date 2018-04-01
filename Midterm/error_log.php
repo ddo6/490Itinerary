d80431476b687e3fd26617d43a8658d5119515ef
JoeTobon <jet33@njit.edu>
2018-04-01 15:35:53 -0400

    Create error_log.php

---
+++ Midterm/error_log.php
@@ -0,0 +1,9 @@
+<?php
+
+  //include in all php files
+  ini_set('display_errors', 1);
+  ini_set('log_errors', 1);
+  ini_set('error_log', dirname(__FILE__).'/log.txt');
+  error_reporting(E_ALL);
+
+?>
