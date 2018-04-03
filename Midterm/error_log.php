ae093cbfe0eb78eb7d0700aef4804717294deda0
JoeTobon <jet33@njit.edu>
2018-04-02 20:07:17 -0400

    Update error_log.php

--- Midterm/error_log.php
+++ Midterm/error_log.php
@@ -2,8 +2,8 @@
   //include in all php files
   ini_set('display_errors', 1);
   ini_set('log_errors', 1);
-  ini_set('error_log','/tmp/log.txt');
+  ini_set('error_log','/tmp/errorLog.txt');
   error_reporting(E_ALL);
-  error_logging("\n");
+  error_log("\n");
 ?>
 
