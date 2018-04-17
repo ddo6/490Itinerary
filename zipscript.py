14a71990edf0e746e35a79c28cce750a6d20056c
ddo6 <ddo6@njit.edu>
2018-04-17 13:35:58 -0400

    Create zipscript.py
    Will update functionality to include unziping to directory. Currently allows you to Specify the file name.

---
+++ zipscript.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import zipfile
+import sys
+import os
+
+
+
+version = input("What version is this? (Please contact admin if unsure) :")
+
+def zip_folder(folder_path, output_path):
+    """Zip the contents of an entire folder (with that folder included
+    in the archive). Empty subfolders will be included in the archive
+    as well.
+    """
+    parent_folder = os.path.dirname(folder_path)
+    
+    # Retrieve the paths of the folder contents.
+    contents = os.walk(folder_path)
+    try:
+        zip_file = zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED)
+        for root, folders, files in contents:
+            # Include all subfolders, including empty ones.
+            for folder_name in folders:
+                absolute_path = os.path.join(root, folder_name)
+                relative_path = absolute_path.replace(parent_folder + '\\',
+                                                      '')
+                print "Adding '%s' to archive." % absolute_path
+                zip_file.write(absolute_path, relative_path)
+            for file_name in files:
+                absolute_path = os.path.join(root, file_name)
+                relative_path = absolute_path.replace(parent_folder + '\\',
+                                                      '')
+                print "Adding '%s' to archive." % absolute_path
+                zip_file.write(absolute_path, relative_path)
+        print "'%s' created successfully." % output_path
+    except IOError, message:
+        print message
+        sys.exit(1)
+    except OSError, message:
+        print message
+        sys.exit(1)
+    except zipfile.BadZipfile, message:
+        print message
+        sys.exit(1)
+    finally:
+        zip_file.close()
+
+## TEST ##
+if __name__ == '__main__':
+#Change the folder path here to the folder you want to zip	
+  zip_folder(r'/home/dina/phpscripts',
+               r'/home/dina/testdirectory'+str(version)+'.zip')
+#	zip_folder(r'folder_path',
+#               r'/home/dina/testdirectory.zip')
