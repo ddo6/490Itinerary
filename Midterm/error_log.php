9383e9c5c8ae705b2dc9606c376b29c32e11bbd3
ddo6 <ddo6@njit.edu>
2018-04-24 11:23:18 -0400

    Delete error_log.php

--- Midterm/error_log.php
+++
@@ -1,9 +0,0 @@
-<?php
-  //include in all php files
-  ini_set('display_errors', 1);
-  ini_set('log_errors', 1);
-  ini_set('error_log','/tmp/errorLog.txt');
-  error_reporting(E_ALL);
-  error_log("End of Error!\n");
-?>
-
